d8e969a2b6bed420c2d4634649ca06ec2180ed52
gabe <gabriel.jsh@gmail.com>
2026-02-03 11:35:40 -0500

    moved backend

--- infra/workloads/dev/VSTShop/backend.tf
+++ infra/workloads/dev/VSTShop/backend.tf
@@ -1,14 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "gabriel-tf-state-2026"
-    key            = "dev/vstshop/terraform.tfstate"
+    key            = "workloads/dev/vstshop/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
-
-    # This is the correct way to specify the cross-account role
-    assume_role = {
-      role_arn = "arn:aws:iam::086739225244:role/github-actions-oidc-role"
-    }
   }
 }
--- infra/workloads/dev/VSTShop/provider.tf
+++ infra/workloads/dev/VSTShop/provider.tf
@@ -1,16 +1,10 @@
 # The Default Provider (Ohio)
 provider "aws" {
   region = var.region
-  assume_role {
-    # This tells Terraform: "Once GitHub logs me in, 
-    # immediately switch to this powerful execution role."
-    role_arn = "arn:aws:iam::195481994910:role/terraform-execution-role-dev"
-  }
 }
 
 # The CloudFront/Certificate Provider (N. Virginia)
 provider "aws" {
   alias   = "us_east_1" # This is the "nickname"
   region  = "us-east-1"
-  profile = "dev"
 }
