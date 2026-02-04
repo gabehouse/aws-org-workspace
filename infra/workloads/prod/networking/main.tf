17560fb8cfabe72c8e3c9da2f6ba775b4a671a5f
gabe <gabriel.jsh@gmail.com>
2026-02-04 08:05:57 -0500

    pre-apply checks

---
+++ infra/workloads/prod/networking/main.tf
@@ -0,0 +1,15 @@
+module "networking" {
+  source = "../../../modules/networking"
+
+  region         = "us-east-2"
+  vpc_name       = "phoenix-prod-vpc"
+  vpc_cidr       = "10.1.0.0/16"
+  public_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  intra_subnets  = ["10.1.10.0/24", "10.1.11.0/24"]
+
+  tags = {
+    Project     = "Phoenix"
+    Environment = "prod"
+    ManagedBy   = "Terraform"
+  }
+}
