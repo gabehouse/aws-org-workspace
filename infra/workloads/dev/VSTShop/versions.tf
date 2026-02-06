86f9fd5674a646ca9872184da0a1abb4ccfdf617
gabehouse <gabriel.jsh@gmail.com>
2026-02-06 11:42:51 +0000

    providers

--- infra/workloads/dev/VSTShop/versions.tf
+++ infra/workloads/dev/VSTShop/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Fixes 'terraform_required_providers'
+      version = "~> 6.0" # Fixes 'terraform_required_providers'
     }
   }
 }
