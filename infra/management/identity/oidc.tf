be4e6fd101142f12775603feee31008301d187b5
gabehouse <gabriel.jsh@gmail.com>
2026-02-06 08:27:17 +0000

    oidc sts:tag perm

--- infra/management/identity/oidc.tf
+++ infra/management/identity/oidc.tf
@@ -75,7 +75,7 @@ resource "aws_iam_role_policy" "github_actions_assume_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action   = "sts:AssumeRole"
+        Action   = ["sts:AssumeRole", "sts:TagSession"]
         Effect   = "Allow"
         Resource = "arn:aws:iam::*:role/terraform-execution-role-*"
       }
