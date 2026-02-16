f1fa52bb7c8e0b7aa8c5f88808289bb69470b25f
gabehouse <gabriel.jsh@gmail.com>
2026-02-16 23:20:29 +0000

    vst storage infra2

--- infra/workloads/prod/vstshop/main.tf
+++ infra/workloads/prod/vstshop/main.tf
@@ -5,7 +5,7 @@ module "globals" {
 module "frontend" {
   source       = "../../../modules/vstshop/frontend"
   project_name = "vstshop"
-  environment  = var.env
+  environment  = var.environment
 }
 
 moved {
@@ -15,7 +15,7 @@ moved {
 
 module "auth" {
   source       = "../../../modules/vstshop/auth"
-  environment  = var.env
+  environment  = var.environment
   project_name = "vstshop"
   unique_id    = "vst-prod-123" # To ensure unique Cognito domain
 
@@ -29,10 +29,11 @@ moved {
 }
 
 module "backend" {
-  source        = "../../../modules/vstshop/backend"
-  environment   = var.env
-  project_name  = "vstshop"
-  user_pool_arn = module.auth.user_pool_arn # Passing the ARN here
+  source          = "../../../modules/vstshop/backend"
+  environment     = var.environment
+  project_name    = "vstshop"
+  user_pool_arn   = module.auth.user_pool_arn # Passing the ARN here
+  vst_bucket_name = module.storage.vst_bucket_name
 }
 
 moved {
@@ -40,9 +41,16 @@ moved {
   to   = module.backend
 }
 
+module "storage" {
+  source      = "../../../modules/vstshop/storage"
+  domain_name = module.frontend.website_url
+  environment = var.environment
+}
+
+
 # This tells Terraform to write a file on your local machine
 resource "local_file" "env_file" {
-  filename = "${path.module}/../../../../services/vstshop/frontend/.env"
+  filename = "${path.module}/../../../../services/vstshop-frontend/.env"
   content  = <<-EOT
     VITE_AWS_REGION=${module.globals.region}
     VITE_USER_POOL_ID=${module.auth.user_pool_id}
