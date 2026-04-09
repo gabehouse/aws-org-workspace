b8931ec27987741cb2b26da40afcb75c5c33c344
gabehouse <gabriel.jsh@gmail.com>
2026-04-09 20:27:01 +0000

    config

--- services/tennis-site/vite.config.ts
+++ services/tennis-site/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // This forces IPv4 instead of the IPv6 loopback
-    port: 5173,
+    port: 5174,
     strictPort: true, // Prevents Vite from jumping to 5174 if 5173 is "busy"
   }
 })
