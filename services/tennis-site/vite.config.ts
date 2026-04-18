8fd7b68ee0951f799e6da5af15e68c2e1ab3b101
Gabe House <gabriel.jsh@gmail.com>
2026-04-18 04:25:08 +0000

    wilderchess

--- services/tennis-site/vite.config.ts
+++ services/tennis-site/vite.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
     host: '0.0.0.0', // This forces IPv4 instead of the IPv6 loopback
     port: 5174,
     strictPort: true, // Prevents Vite from jumping to 5174 if 5173 is "busy"
+    watch: {
+      usePolling: true,
+      interval: 100, // Check every 100ms
+    },
   }
+
 })
